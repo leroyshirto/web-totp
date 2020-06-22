cfa1fe3dfdacc1d9ed8d9799c72257cf806f3ece
Leroy Shirto <leroy.shirto@fasthosts.com>
2020-06-22 11:39:28 +0100

    add service worker update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,22 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import { SnackbarProgrammatic as Snackbar } from 'buefy'
+
+const notifyUserAboutAnUpdate = (worker) => {
+  if (worker === null) {
+    throw new Error('notifyUserAboutAnUpdate: Service worker was null')
+  }
+
+  Snackbar.open({
+    message: 'A new version of web-totp is available',
+    actionText: 'Update',
+    indefinite: true,
+    onAction: () => {
+      worker.postMessage({ action: 'skipWaiting' })
+    }
+  })
+}
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -15,18 +31,31 @@ if (process.env.NODE_ENV === 'production') {
     },
     cached () {
       console.log('Content has been cached for offline use.')
+      Snackbar.open('web-totp has been cached for offline use.')
     },
     updatefound () {
       console.log('New content is downloading.')
     },
-    updated () {
+    updated (registration) {
       console.log('New content is available; please refresh.')
+      notifyUserAboutAnUpdate(registration.waiting)
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      const message = 'No internet connection found. App is running in offline mode.'
+      console.log(message)
+      Snackbar.open(message)
     },
     error (error) {
-      console.error('Error during service worker registration:', error)
+      const message = `Error during service worker registration: ${error}`
+      console.error(message)
+      Snackbar.open(message)
     }
+  })
+
+  let refreshing = false
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (refreshing) return
+    window.location.reload()
+    refreshing = true
   })
 }
